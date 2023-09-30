7cd86030e21a0d8e3d78f7d99b18ad0f0ef09adf
lazyPace <brandonprater84@yahoo.com>
2023-09-29 23:10:23 -0400

    initial commit/added background and basic header

---
+++ src/Spellbook.js
@@ -0,0 +1,119 @@
+// List of all spells
+
+const spellbook = [
+  {
+    incantation: 'Accio',
+    nickname: 'Summoning Charm',
+    effect:
+      'Summons an object towards the caster. Able to summon objects within line of sight and out of view objects.',
+    type: 'Charm',
+    restricted: false,
+    difficulty: '3'
+  },
+  {
+    incantation: 'Aguamenti',
+    nickname: 'Water-Making Spell',
+    effect: 'Produces a clean, drinkable jet of water from the wand tip.',
+    type: 'Conjuration',
+    restricted: false,
+    difficulty: '2'
+  },
+  {
+    incantation: 'Alohomora',
+    nickname: 'Unlocking Charm',
+    effect: 'Unlocks doors and other locked objects. Can be bewitched against.',
+    type: 'Charm',
+    restricted: false,
+    difficulty: '2'
+  },
+  {
+    incantation: 'Anapneo',
+    effect: "Clear's the target's airway if they are choking on something.",
+    type: 'Healing',
+    restricted: false,
+    difficulty: '4'
+  },
+  {
+    incantation: 'Aparecium',
+    nickname: 'Revealing Charm',
+    effect:
+      'Reveals secret messages written in invisible ink, or any other hidden markings. Works against concealing charms.',
+    type: 'Charm',
+    restricted: false,
+    difficulty: '3'
+  },
+  {
+    incantation: 'Arresto Momentum',
+    nickname: 'Slowing Charm',
+    effect:
+      'Decreases the velocity of a moving target. Can be cast on oneself.',
+    type: 'Charm',
+    restricted: false,
+    difficulty: '5'
+  },
+  {
+    incantation: 'Ascendio',
+    effect: 'Lifts the caster high into the air.',
+    type: 'Charm',
+    restricted: false,
+    difficulty: '3'
+  },
+  {
+    incantation: 'Avada Kedavra',
+    nickname: 'Killing Curse',
+    effect:
+      'Causes instantaneous death with no known counter-curse. Accompanied by a flash of green light and a rushing noise.',
+    type: 'Curse',
+    restricted: true,
+    difficulty: '10'
+  },
+  {
+    incantation: 'Avifors',
+    effect: 'Transforms target into a bird',
+    type: 'Transfiguration',
+    restricted: false,
+    difficulty: '3'
+  },
+  {
+    incantation: 'Avis',
+    nickname: 'Bird-Conjuring Charm',
+    effect: 'Conjures a flock of birds from the tip of the wand.',
+    type: 'Conjuration',
+    restricted: false,
+    difficulty: '4'
+  },
+  {
+    incantation: 'Bombarda',
+    nickname: 'Exploding Charm',
+    effect:
+      'Provokes a small explosion. Can append Maxima to end of spell to increase strength.',
+    type: 'Charm',
+    restricted: false,
+    difficulty: '3'
+  },
+  {
+    incantation: 'Calvorio',
+    nickname: 'Hair Loss Curse',
+    effect: "Removes the victim's hair.",
+    type: 'Curse',
+    restricted: false,
+    difficulty: '4'
+  },
+  {
+    incantation: 'Cantis',
+    effect:
+      'Causes the victim to burst into song. Used by Hogwarts professors to enchant suits of armor to sing.',
+    type: 'Charm',
+    restricted: false,
+    difficulty: '5'
+  },
+  {
+    incantation: 'Capacious extremis',
+    nickname: 'Extension Charm',
+    effect:
+      'Expands the internal dimensions of an object without affecting the external dimensions.',
+    type: 'Charm',
+    restricted: true,
+    difficulty: '8'
+  }
+]
---
+++ src/TableOfContents.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function TableOfContents () {
+  const spellTypes = [
+    'Charms',
+    'Conjurations',
+    'Counter-Spells',
+    'Curses',
+    'Healing Spells',
+    'Hexes',
+    'Jinxes',
+    'Transfigurations'
+  ]
+}
+
+export default TableOfContents
